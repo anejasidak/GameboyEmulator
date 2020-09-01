e27ea442e745f6103ae96ad40ba28289e72e0d84
anejasidak <anejasidak@gmail.com>
2020-09-01 15:59:27 -0700

    Added rl instruction tempatlate

--- src/cb.c
+++ src/cb.c
@@ -3,6 +3,32 @@
 #include <assert.h>
 #include <stdio.h>
 
+static void rl(uint8_t *reg)
+{
+    uint8_t carry = (*reg & 0x80) >> 7;
+    *reg <<= 1;
+    *reg += carry;
+
+    if (carry)
+    {
+        setFlag(FLAGS_CARRY);
+    }
+    else
+    {
+        resetFlag(FLAGS_CARRY);
+    }
+
+    if (*reg)
+    {
+        resetFlag(FLAGS_ZERO);
+    }
+    else
+    {
+        setFlag(FLAGS_ZERO);
+    }
+    resetFlag(FLAGS_HALFCARRY | FLAGS_HALFCARRY);
+}
+
 static void bit(uint8_t b, uint8_t registerValue)
 {
     if (((1 << b) & registerValue) == 0)
@@ -21,7 +47,12 @@ void cpuExecuteCB(struct s_cpu *cpu, uint32_t opcode)
 
     switch (opcode & 0x00ff0000)
     {
-    case 0x007c0000:
+    case 0x00110000:
+    {
+        rl(&cpu->c);
+    }
+    break;
+    case 0x007C0000:
     {
         bit(7, cpu->h);
     }
